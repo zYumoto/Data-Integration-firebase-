235e81252c7b2dd52e759d2b45e174af8d17e4d4
Victor Juan <victorjuanrazer@gmail.com>
2022-11-22 13:48:45 -0300

    add:layout componets

--- src/pages/Estiloso.jsx
+++ src/pages/Estiloso.jsx
@@ -1,9 +1,12 @@
 import styles from "../styles/Estiloso.module.css";
+import Layout from "../components/Layout";
 
 export default function Estiloso() {
   return (
-    <div className={styles.roxo}>
-      <h1>Estilo usando CSS Modulos</h1>
-    </div>
+    <Layout titulo="Exemplo de CSS Modularizado">
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Modulos</h1>
+      </div>
+    </Layout>
   );
 }
--- src/pages/api/index.jsx
+++ src/pages/api/index.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Fundamentos de next.Js & React</h1>
-      <h2>Vamos estudar esse framework </h2>
-      </div>
+      <Link href="/estiloso">Estiloso</Link>
+    </div>
   );
 }
