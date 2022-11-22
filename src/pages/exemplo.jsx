8ae74f19b17fb194a0c584bcfa24f6ea23b4bd70
Victor Juan <victorjuanrazer@gmail.com>
2022-11-22 13:47:12 -0300

    add:component properties

---
+++ src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+  return (
+    <>
+      <Cabecalho titulo="Next.js & React" />
+      <Cabecalho titulo="Aprender Next na pratica" />
+    </>
+  );
+}
---
+++ src/pages/jsx.jsx
@@ -0,0 +1,13 @@
+export default function Jsx() {
+    const titulo = <h1>JSX é um Conceito Central</h1>;
+    function subtitulo() {
+      return <h2>{"muito legal".toUpperCase()}</h2>;
+    }
+    return (
+      <div>
+        {titulo}
+        {subtitulo()}
+        <p>{JSON.stringify({ nome: "João", idade: 30 })}</p>
+      </div>
+    );
+  }
